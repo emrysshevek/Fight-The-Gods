3b741fbae997b9da3ab19c7cb291180b26774d44
Emrys Shevek <emrysshevek@gmail.com>
2025-01-30 23:16:53 -0800

    fixed spinning tween transitions

--- scenes/boss_2_battle.gd
+++ scenes/boss_2_battle.gd
@@ -1,12 +1,13 @@
 extends Node2D
 
+@export var spin_speed := 1.0
 
 @onready var boss: Boss2 = $Enemy
 @onready var camera: Camera = $Camera
 @onready var world: Node2D = $world
 
 func _ready() -> void:
-	boss.stomp.connect(_on_boss_stomp)
+	# boss.stomp.connect(_on_boss_stomp)
 	boss.slam.connect(_on_boss_slam)
 
 func _on_boss_stomp() -> void:
@@ -14,9 +15,9 @@ func _on_boss_stomp() -> void:
 
 func _on_boss_slam() -> void:
 	# print("Boss slammed stage...spin time!")
-	var spin_amount = world.rotation + randi_range(10, 14) * (PI/2)
+	var spin_amount = world.rotation + randi_range(3,4) * (PI)
+	var spin_duration = (spin_amount - world.rotation) * (1 / spin_speed)
 	var tween = create_tween()
-	tween.tween_property(world, "rotation", spin_amount, 3)
 	tween.set_ease(Tween.EASE_OUT)
-	
-
+	tween.set_trans(Tween.TRANS_SPRING)
+	tween.tween_property(world, "rotation", spin_amount, spin_duration)
