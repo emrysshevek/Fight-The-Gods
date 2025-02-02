d93595039467222f8a68e254e4dc3738070ba0fc
Emrys Shevek <emrysshevek@gmail.com>
2025-02-02 08:37:28 -0800

    simple menu animations

--- scenes/main_menu/main_menu.gd
+++ scenes/main_menu/main_menu.gd
@@ -8,7 +8,9 @@ func _ready() -> void:
 	Engine.time_scale = 1
 
 func _on_play_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/boss2_battle/boss2_battle.tscn")
+	$AnimationPlayer.play("transition")
+	$AnimationPlayer.animation_finished.connect(_on_animation_finished)
+	
 
 func _on_options_button_pressed() -> void:
 	$UI/Control/Options.show()
@@ -24,3 +26,7 @@ func _on_bonus_button_mouse_exited() -> void:
 
 func _on_bonus_button_mouse_entered() -> void:
 	pass # Replace with function body.
+
+func _on_animation_finished(name: StringName) -> void:
+	if name == "transition":
+		get_tree().change_scene_to_file("res://scenes/boss2_battle/boss2_battle.tscn")
