a17d88090b3d86c3a6422a1787d60a026996d953
Emrys Shevek <emrysshevek@gmail.com>
2025-02-02 00:41:23 -0800

    checkpoint, semifinal build before jam deadline!

--- components/globals/globals.gd
+++ components/globals/globals.gd
@@ -12,11 +12,6 @@ signal spin_ended()
 var spinning = false
 var difficulty_multiplier := 0.0
 
-func _ready() -> void:
-	var game_status = load_from_file()
-	if game_status != null and game_status == "won":
-		game_won = true
-	print(game_won)
 
 func set_difficulty(difficulty: String) -> void:
 	match difficulty:
@@ -38,17 +33,3 @@ func end_spin() -> void:
 
 func beat_game() -> void:
 	game_won = true
-	save_to_file("won")
-
-func save_to_file(content):
-	var file = FileAccess.open("user://save_game.dat", FileAccess.WRITE)
-	file.store_string(content)
-
-func load_from_file():
-	var file = FileAccess.open("user://save_game.dat", FileAccess.READ)
-	if file == null:
-		print(error_string(FileAccess.get_open_error()))
-		return 
-		
-	var content = file.get_as_text()
-	return content
