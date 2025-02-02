fb3b01b1ac83dadb0b4b7ccfe0481c14d89b4638
Emrys Shevek <emrysshevek@gmail.com>
2025-02-02 09:00:21 -0800

    penultimate build checkpoint

--- ui/img_button.gd
+++ ui/img_button.gd
@@ -1,9 +1,21 @@
 class_name ImgButton
 extends TextureButton
 
-# func _process(delta: float) -> void:
-#     if disabled:
-#         modulate = Color(1,1,1,.75)
-#     else:
-#         print("undisabled")
-#         modulate = Color(1,1,1,1)
+var elapsed := 0.0
+var a := 4
+var f := .4
+var p := randf_range(0, 2 * PI)
+
+var base_position: Vector2
+
+func _ready() -> void:
+    base_position = position
+
+func _process(delta: float) -> void:
+    elapsed += delta
+
+    var offset = a * sin(2 * PI * f * elapsed + p)
+    position.y = base_position.y + offset
+
+func restart() -> void:
+    base_position = position
